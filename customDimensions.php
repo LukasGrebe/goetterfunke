5e5d5e326434c7d9d62668c90e338de36b2dca37
Lukas Grebe <git@lukas.grebe.me>
2020-03-30 09:26:36 +0200

    Small Batches

--- customDimensions.php
+++ customDimensions.php
@@ -5,75 +5,52 @@ function getCustomDimensions($analyticsService, $account, $property){
 	return $customDimensions->getItems();
 }
 
-function parseFromJSON($json){
-	function arrayToDimension($arr){
-		$_properties = array('Scope','Active','Index');
-		$cd = new Google_Service_Analytics_CustomDimension();
-		$cd->setScope($arr['scope']);
-		$cd->setActive($arr['active']);
-		$cd->setIndex($arr['index']);
-		return $cd;
-	}
-	return array_map('arrayToDimension', json_decode($json,true));
+function arrayToDimensionObject($arr){
+	$cd = new Google_Service_Analytics_CustomDimension();
+	$cd->setScope($arr['scope']);
+	$cd->setActive($arr['active']);
+	$cd->setIndex($arr['index']);
+	return $cd;
 }
 
-
-function createChangeRequets($analyticsService,$currentCDconfiguration,$targetCDconfiguration){
-	$requests = [];
+function configDiffGenerator($currentCDconfiguration,$targetCDconfiguration){
+	echo "hello world\n";
 	foreach ($currentCDconfiguration as $cd) {
 		$index = $cd->getIndex();
+		$targetCDIndex = $index-1; //because unlike Analytic's CDs, arrays are 0 based
 		$changeset = [];
 		
-		if(!array_key_exists($index-1, $targetCDconfiguration)){
+		if(!array_key_exists($targetCDIndex, $targetCDconfiguration)){
+			//this CD should not be active
 			if($cd->getActive()){
 				$cd->setActive(false);
-				$update = true;
-				$changeset[] = 'status (set to inactive)';
+				echo "Deactivate ga:dimension$index\n";
+				yield 'patch' => $cd;
 			}
 		}else{
-			//check if active, name and scope are set as they should be
-			$should = $targetCDconfiguration[$index-1];
+			$target = $targetCDconfiguration[$targetCDIndex];
 
-			if($should['index'] != $index){
+			if($target['index'] != $index){
 				var_dump($should);
-				throw new Exception("Configuration index {$should['index']} (see above) does not match array position {$index}");
+				throw new Exception("Configuration index {$target['index']} (see above) does not match array position {$index}");
 			}
 
-			if(!$cd->getActive() != $should['active']){
-				$cd->setActive($should['active']);
-				$update = true;
-				$changeset[] = 'status ('.($should['active']?'t':'f').')';
+			$target['scope'] = strtoupper($target['scope']);
+			//check if active, name and scope are set as they should be
+			if( $cd->getActive() != $target['active'] or
+				$cd->getName()   != $target['name'] or
+				$cd->getScope()  != $target['scope']){
+					echo "Update ga:dimension$index: {$cd->getActive()} to {$target['active']}, ";
+					echo "{$cd->getName()} to {$target['name']}, ";
+					echo "{$cd->getScope()} to {$target['scope']}\n";
+					yield 'patch' => arrayToDimensionObject($target);
 			}
-	
-			if($cd->getName() != $should['name']){
-				$cd->setName($should['name']);
-				$update = true;
-				$changeset[] = "name ({$should['name']})";
-			}
-	
-			if($cd->getScope() != $should['scope']){
-				$cd->setScope($should['scope']);
-				$update = true;
-				$changeset[] = "scope ({$should['scope']})";
-			}
-		}
-		if($update){
-			echo 'updating ' . implode(', ', $changeset) . " of ga:dimension$index\n";
-			$requests[$index] = $analyticsService->management_customDimensions->patch($account, $property, ('ga:dimension' . $index), $cd);
-		}else{
-			echo "ga:dimension$index ok\n";
-		}
-		// remove dimension from known dimensions so its easy to add any remaning after this loop
-		unset($dimensionsSetup[$index-1]);
+			// remove dimension from known dimensions so its easy to add any remaning after this loop
+			unset($targetCDconfiguration[$targetCDIndex]);
+		}		
 	}
-
-	foreach($dimensionsSetup AS $index=>$should){
-		$cd = new Google_Service_Analytics_CustomDimension();
-		$cd->setActive($should['active']);
-		$cd->setName($should['name']);
-		$cd->setScope($should['scope']);
-	
-		$requests[$index+1] = $analyticsService->management_customDimensions->insert($account, $property, $cd);
+	// any non-removed target configurations left?
+	foreach($targetCDconfiguration as $target){
+		yield 'insert' => arrayToDimensionObject($target);
 	}
-	return $requests;
 }
--- goetterfunke.php
+++ goetterfunke.php
@@ -4,6 +4,8 @@ error_reporting(E_ALL);
 
 if (php_sapi_name() != 'cli') {
     throw new Exception('This application must be run on the command line.');
+}else{
+    echo "hi\n";
 }
 
 require 'vendor/autoload.php';
@@ -14,24 +16,68 @@ require_once 'customDimensions.php';
 $client = getClient('client_credentials.json');
 $analyticsService = new Google_Service_Analytics($client);
 
-
-switch($argv[1]){
+list($file,$cliAction,$account,$property,$cliParam) = $argv;
+echo "hello {$cliAction}\n";
+switch($cliAction){
     case 'getCDsJSON':
-        $customDimensions = getCustomDimensions($analyticsService,$argv[2],$argv[3]);
+        $customDimensions = getCustomDimensions($analyticsService,$account,$property);
         print json_encode($customDimensions,JSON_PRETTY_PRINT);
     break;
     case 'setCDs': 
         //get a delta
-        $is = getCustomDimensions($analyticsService,$argv[2],$argv[3]);
-        $should = json_decode(file_get_contents(__DIR__ . '/' . $argv[4]),true);
+        $currentCDconfiguration = getCustomDimensions($analyticsService,$account,$property);
+        $targetCDconfiguration = json_decode(file_get_contents(__DIR__ . '/' . $cliParam),true);
 
+        $change = configDiffGenerator($currentCDconfiguration,$targetCDconfiguration);
+        var_dump($change);
         $client->setUseBatch(true);
-        $changes = createChangeRequets($analyticsService,$is,$should);
-        executeThrottledBatch($client, $analyticsService, $changes);
-        
+        $maxBatchSize = 5;
+
+        while($change->valid()){
+
+            $requests = [];
+            for ($i=0; $i < $maxBatchSize and $change->valid(); $i++) { 
+                $cd = $change->current();
+                if($change->key() == 'patch'){
+                    
+                    $requests[$cd->getIndex()] = $analyticsService->management_customDimensions->patch($account, $property, ('ga:dimension' . $cd->getIndex()),$cd);
+                }else{
+                    $requests[$cd->getIndex()] = $analyticsService->management_customDimensions->insert($account, $property, $cd);
+                }
+                $change->next();
+            }
+
+            do{
+                echo "create new batch \n";
+                $batch = $analyticsService->createBatch();
+                foreach($requests as $index => $request){
+                    $batch->add($request, $index);
+                }
+                $results = $batch->execute();
+                foreach ($results as $result => $obj) {
+                    $index = substr($result, 9);
+                    if(get_class($obj) !== "Google_Service_Exception" ){
+                        unset($requests[$index]);
+                        echo "CD $index ok\n";
+                        var_dump($obj);
+                    }else{
+                        $msg = json_decode($obj->getMessage());
+                        echo "Retry CD $index:{$obj->getMessage()}\n";
+                        sleep(3);
+                    }
+                }
+            }while(count($requests)>0);
+            echo "Section completed Successfully.\n\n";
+        }
     break;
     case 'listProperties':
         listProperties($analyticsService);
     break;
+    default:
+        echo "unknown Action {$cliAction}\n";
+        echo "use\n";
+        echo "listProperties\n";
+        echo "getCDsJSON <account> <property>\n";
+        echo "setCDs <account> <property> <config.json>\n";
 }
 
