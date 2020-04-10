e88adc0df372de3571b0e28716daf064012aa3bc
Lukas Grebe <git@lukas.grebe.me>
2020-04-10 22:27:23 +0200

    make cli output less noisy

--- customDimensions.php
+++ customDimensions.php
@@ -19,14 +19,16 @@ function configDiffGenerator($currentCDconfiguration,$targetCDconfiguration){
 		$index = $cd->getIndex();
 		$targetCDIndex = $index-1; //because unlike Analytic's CDs, arrays are 0 based
 		$changeset = [];
-		echo "{$index}: ";
+		echo "{$index}";
 		if(!array_key_exists($targetCDIndex, $targetCDconfiguration)){
 			//this CD should not be active
 			if($cd->getActive()){
 				$cd->setActive(false);
 				$cd->setName('-not in use-');
-				echo "set active: false; change name from '{$cd->getName()}' to '-not in use-'; ";
+				echo " set active: false, change name from '{$cd->getName()}' to '-not in use-' ";
 				yield 'patch' => $cd;
+			}else{
+				echo "\033[32mok\033[0m ";
 			}
 		}else{
 			$target = $targetCDconfiguration[$targetCDIndex];
@@ -42,13 +44,13 @@ function configDiffGenerator($currentCDconfiguration,$targetCDconfiguration){
 			$updateFlag = false;
 			if( $cd->getActive() != $target['active'] ){
 				$updateFlag = true;
-				echo "set active " & $target['active']? 'true':'false' & "; ";
+				echo " set active " . $target['active']? 'true':'false' . ", ";
 			}if( $cd->getName()   != $target['name'] ){
 				$updateFlag = true;
-				echo "change name from '{$cd->getName()}' to '{$target['name']}'; ";
+				echo " change name from '{$cd->getName()}' to '{$target['name']}', ";
 			}if( $cd->getScope()  != $target['scope'] ){
 				$updateFlag = true;
-				echo "change scope from '{$cd->getScope()}' to '{$target['scope']}'; ";
+				echo " change scope from '{$cd->getScope()}' to '{$target['scope']}', ";
 			}
 			if($updateFlag){
 				yield 'patch' => arrayToDimensionObject($target);
@@ -62,7 +64,7 @@ function configDiffGenerator($currentCDconfiguration,$targetCDconfiguration){
 	// any non-removed target configurations left?
 	foreach($targetCDconfiguration as $target){
 		$target = arrayToDimensionObject($target);
-		echo "create with name '{$target->getName()}'; scope '{$target->getScope()}'; '";
+		echo " create with name '{$target->getName()}'; scope '{$target->getScope()}'; ";
 		yield 'insert' => $target;
 	}
 }
